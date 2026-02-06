7033e5e8f684c8d7ab57811baa94a2bfe4c565e8
kyletho <k.hecker92@gmail.com>
2026-02-06 13:29:15 -0500

    finished read_line

--- shell.c
+++ shell.c
@@ -1,13 +1,37 @@
-int read_line() {
+char *read_line() {
 
-    int buffSpace = 1048;
-    char *buffer = buffSpace;
+    int buff_space = 1048;
+    int index;
+    char *buffer = malloc(buff_space);
     int c;
 
-    while(1) {
-    
+    if (!buffer) {
+        fprintf(stderr, "Memory allocation error");
+        exit(EXIT_FAILURE);
+    }
 
-    
+    while (1) {
+
+        c = get_char();
+
+        if (c == EOF || c == \n) {
+            buffer[index] = "\0";
+            return *buffer;
+        } else {
+            buffer[index] = c;
+        }
+
+        index++;
+
+        if (index >= buff_space) {
+            buff_space += 1024;
+            buffer = realloc(buffer, buff_space);
+            if (!buffer) {
+                fprintf(stderr, "Memory reallocation error");
+                exit(EXIT_FAILURE);
+            }
+        }
+
     }
 
 
